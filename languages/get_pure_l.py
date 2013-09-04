ae0843182197d52fcd36584001fd5bc4b17be686
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2013-09-04 15:11:00 +0200

    get_pure_l.py too long line broken for smaller

--- languages/get_pure_l.py
+++ languages/get_pure_l.py
@@ -5,7 +5,9 @@ with open(fname) as f:
     content = f.readlines()
     for c in content:
         #print c
-        if (c.strip().endswith(':') == True) and (c.count('aliases') < 1) and (c.count('extensions') < 1) and (c.count('filenames') < 1):
+        if (c.strip().endswith(':') == True) and 
+        (c.count('aliases') < 1) and (c.count('extensions') < 1) 
+        and (c.count('filenames') < 1):
             print c
             how_many = how_many + 1;
 print how_many
