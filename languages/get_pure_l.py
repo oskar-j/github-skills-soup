06487e6471f4fcf311cba86d5f53f84fe690925a
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2013-09-09 15:04:02 +0200

    all languages parsed to all-languages.csv

--- languages/get_pure_l.py
+++ languages/get_pure_l.py
@@ -1,13 +1,15 @@
+import csv
+
 fname = 'languages.yml'
 how_many = 0
 
-with open(fname) as f:
-    content = f.readlines()
-    for c in content:
-        #print c
-        if (c.strip().endswith(':') == True) and 
-        (c.count('aliases') < 1) and (c.count('extensions') < 1) 
-        and (c.count('filenames') < 1):
-            print c
-            how_many = how_many + 1;
+with open('all-languages.csv', 'wb') as csvfile:
+    outputter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    with open(fname) as f:
+        content = f.readlines()
+        for c in content:
+            if (c.strip().endswith(':') == True) and (c.count('aliases') < 1) and (c.count('extensions') < 1) and (c.count('filenames') < 1):
+                outputter.writerow([str(c.strip().replace(':',''))])
+                how_many = how_many + 1
 print how_many
+csvfile.close()
